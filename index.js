01a7ce6d2710761e0469ad02693187f5d781b64a
Vitaliy Vasylykiv <92222356+Plupiks@users.noreply.github.com>
2022-01-11 19:20:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inputSlider = document.querySelector("input")
 inputSlider.oninput = function () {
   const spanValue = inputSlider.value;
   slideValue.textContent = spanValue;
-  slideValue.style.left = `${20 + (this.clientWidth - 20) / 100 * (spanValue / 5000)}px`;
+  slideValue.style.left = `${20 + (this.clientWidth - 20) / 100 * (spanValue / 2)}px`;
   slideValue.classList.add("show");
   console.log(slideValue);
 };
